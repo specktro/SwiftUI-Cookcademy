7e4db1b439cc56c34a7b3d323d484dc2cfcb358d
@specktro <specktro@nonull.mx>
2024-10-19 21:05:18 -0600

    Initial commit with gitignore file template from github

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by specktro on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
