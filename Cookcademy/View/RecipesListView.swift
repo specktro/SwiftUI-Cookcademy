3376e13c896fceb62865d61dca3658a182e1e946
specktro <specktro@nonull.mx>
2024-10-21 11:48:23 -0600

    feat: Add a RecipeDetailView wand refactor view colors

--- Cookcademy/View/RecipesListView.swift
+++ Cookcademy/View/RecipesListView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct RecipesListView: View {
     @StateObject private var recipeData = RecipeData()
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
     
     var body: some View {
         List {
             ForEach(recipeData.recipes) { recipe in
-                Text(recipe.mainInformation.name)
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
         .navigationTitle("All recipes")
     }
