da01e8fc480d78af809d2eb09bc8860f35246d3e
@specktro <specktro@nonull.mx>
2024-11-06 18:18:13 -0600

    feat: Add @AppStorage persistance to property colors in settings view

--- Cookcademy/View/ExploreRecipes/RecipeDetailView.swift
+++ Cookcademy/View/ExploreRecipes/RecipeDetailView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct RecipeDetailView: View {
     @Binding var recipe: Recipe
     @State private var isPresenting = false
-    private let listBackgroundColor = AppColor.background
-    private let listTextColor = AppColor.foreground
+    @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
+    @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
     
     var body: some View {
         VStack {
---
+++ Cookcademy/View/SettingsView.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsView.swift
+//  Cookcademy
+//
+//  Created by specktro on 05/11/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @State private var hideOptionalSteps: Bool = false
+    @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
+    @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                ColorPicker("List BackgroundColor", selection: $listBackgroundColor)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+                ColorPicker("Text Color", selection: $listTextColor)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+                Toggle("Hide Optional Steps", isOn: $hideOptionalSteps)
+                    .padding()
+                    .listRowBackground(listBackgroundColor)
+            }
+            .foregroundColor(listTextColor)
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
